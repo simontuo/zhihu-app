d67d9f0c5d2e8c4b15d4cf906dcb27be2cc699cd
simontuo <simontuo666@gmail.com>
2017-09-06 22:02:07 +0800

    test git merge master

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// test master
 Route::middleware('api')->get('/topics', 'TopicsController@topics');
 Route::middleware('auth:api')->post('/question/follower', 'QuestionFollowController@follower');
 Route::middleware('auth:api')->post('/question/follow', 'QuestionFollowController@followThisQuestion');
