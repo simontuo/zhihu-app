43324b60104157375527e498edcf14deb1ca8d8f
simontuo <simontuo666@gmail.com>
2017-09-10 20:23:32 +0800

    头像上传七牛

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Storage;
 
 class UsersController extends Controller
 {
@@ -14,10 +15,15 @@ class UsersController extends Controller
     public function changeAvatar(Request $request)
     {
         $file = $request->file('img');
-        $fileName = md5(time().user()->id).'.'.$file->getClientOriginalExtension();
-        $file->move(public_path('avatars'), $fileName);
+        $fileName = '/avatars/'.md5(time().user()->id).'.'.$file->getClientOriginalExtension();
 
-        user()->avatar = '/avatars/'.$fileName;
+        // 上传服务器
+        // $file->move(public_path('avatars'), $fileName);
+        
+        // 上传七牛
+        Storage::disk('qiniu')->writeStream($fileName, fopen($file->getRealPath(), 'r'));
+
+        user()->avatar = 'http://'.config('filesystems.disks.qiniu.domain').'/'.$fileName;
         user()->save();
 
         return ['url' => user()->avatar];
--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Overtrue\LaravelFilesystem\Qiniu\QiniuStorageServiceProvider::class,
 
         /*
          * Application Service Providers...
